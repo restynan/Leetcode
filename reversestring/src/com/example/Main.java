8daf513a3bcbb527085d2bec2b0acac26d249169
RESTY NASIMBWA <54865360+restynan@users.noreply.github.com>
2020-04-15 15:05:18 -0500

    revesestring

---
+++ reversestring/src/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+	ReverseString obj = new ReverseString();
+
+        System.out.println (obj.reverseWithStringBuilder("Nasimbwa"));
+        System.out.println (obj.reverseManually("hello!"));
+
+    }
+}
---
+++ reversestring/src/com/example/ReverseString.java
@@ -0,0 +1,22 @@
+package com.example;
+
+public class ReverseString {
+
+    public String reverseWithStringBuilder(String st)
+    {
+        return    new StringBuilder(st).reverse().toString();
+
+    }
+
+    public String   reverseManually (String s){
+       // StringBuilder st = new StringBuilder();
+   String str="";
+        for (int i=s.length()-1;i>0;i--) {
+           str =   str + s.charAt(i);
+
+        }
+
+        return str;
+
+    }
+}
