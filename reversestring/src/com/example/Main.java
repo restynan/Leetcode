3215f329201561987dbe9d70724e0327a9ae3aa8
RESTY NASIMBWA <54865360+restynan@users.noreply.github.com>
2020-04-15 15:20:24 -0500

    sys

--- reversestring/src/com/example/Main.java
+++ reversestring/src/com/example/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println ("Enter string to reverse : jyy");
         Scanner input  =  new Scanner(System.in);
         String original = input.nextLine();
 
