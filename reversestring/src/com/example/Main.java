ad10d99be9131bd0a3a300ec37b515ba9a49f806
RESTY NASIMBWA <54865360+restynan@users.noreply.github.com>
2020-04-15 15:16:27 -0500

    scanner added

--- reversestring/src/com/example/Main.java
+++ reversestring/src/com/example/Main.java
@@ -1,12 +1,20 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	ReverseString obj = new ReverseString();
 
-        System.out.println (obj.reverseWithStringBuilder("Nasimbwa"));
-        System.out.println (obj.reverseManually("hello!"));
+        Scanner input  =  new Scanner(System.in);
+        String original = input.nextLine();
+
+
+
+        ReverseString obj = new ReverseString();
+
+        System.out.println (obj.reverseWithStringBuilder(original));
+        System.out.println (obj.reverseManually(original));
 
     }
 }
--- reversestring/src/com/example/ReverseString.java
+++ reversestring/src/com/example/ReverseString.java
@@ -11,7 +11,7 @@ public class ReverseString {
     public String   reverseManually (String s){
        // StringBuilder st = new StringBuilder();
    String str="";
-        for (int i=s.length()-1;i>0;i--) {
+        for (int i=s.length()-1;i>=0;i--) {
            str =   str + s.charAt(i);
 
         }
